e61453f9e1dd347e6ab284f50f60c6bed5667422
theCrius <claudio.vallesi@gmail.com>
2018-06-19 11:16:29 +0100

    Some git bullshittery left this out of the commit

--- common/emit.js
+++ common/emit.js
@@ -3,17 +3,17 @@
  * @param {req} req , The client request.
  * @param {string} label , The event name to identify the message
  */
-function broadcast(req, label) {
-  const io = req.webSocket.io ? req.webSocket.io : req.webSocket;
+function emit(req, label) {
+  const io = req.courierSocket.io ? req.courierSocket.io : req.courierSocket;
   const data = req.body.data;
   const namespace = io.of('/' + req.body.from.split('-').pop());
   namespace.emit(label, JSON.stringify(data));
 
   if (process.env.LOG_MESSAGES) {
     // console.log(JSON.stringify(req.headers));
-    console.log(`Trigger invoked from ${req.header('host')} on ${namespace.name}: (${label})\n`, data);
+    console.log(`[ DEBUG ] Trigger invoked from ${req.header ? req.header('host') : 'self'} on ${namespace.name}: (${label})\n`, data);
   }
   return true;
 }
 
-module.exports = broadcast;
+module.exports = emit;
